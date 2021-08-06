c209d0cb3437c99da63ae0b43de765bee603aecc
Max Jöhnk <maxjoehnk@gmail.com>
2021-08-06 06:57:52 +0200

    feat(discovery): don't crash when no announcer for a device can be found

--- src/mqtt/discovery/discovery.ts
+++ src/mqtt/discovery/discovery.ts
@@ -2,10 +2,13 @@ import { Provide, ProvideInterface } from '../../ioc-container';
 import { AnnouncerFactory } from './announcer';
 import { ApplicationInitializer } from '../../lifecycle/application-initializer';
 import { DeviceRegistry } from '../../devices/device-registry';
+import { getLogger } from '../../logger';
 
 @Provide()
 @ProvideInterface(ApplicationInitializer)
 export class HomeassistantDiscovery implements ApplicationInitializer {
+  private static readonly logger = getLogger();
+
   constructor(private announcerFactory: AnnouncerFactory, private deviceRegistry: DeviceRegistry) {}
 
   order = 99;
@@ -14,8 +17,12 @@ export class HomeassistantDiscovery implements ApplicationInitializer {
     const devices = await this.deviceRegistry.getDevices();
 
     for (const device of devices) {
-      const announcer = this.announcerFactory.getAnnouncer(device);
-      await announcer.announce(device);
+      try {
+        const announcer = this.announcerFactory.getAnnouncer(device);
+        await announcer.announce(device);
+      }catch (err) {
+        HomeassistantDiscovery.logger.debug(err);
+      }
     }
   }
 }
